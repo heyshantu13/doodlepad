4b4b8852b0cb4a7032820b38a276983fcf195a68
heyshantu13 <heyshantu13@gmail.com>
2020-02-04 14:10:10 +0530

    fixed no query result error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
 
 	public function search(Request $request){
 		  $user = Auth::user();
-        $profile = UserProfile::where('user_id', $user->id)->firstOrFail();
+        $profile = User::where('id', $user->id)->firstOrFail();
 
           $search = $request->q;
         $profiles = User::where('username', 'like', '%' . $search . '%')->paginate();
