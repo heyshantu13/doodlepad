b4abd972b78f8d3e51f0bf3e275e03abbc1d2f6f
shantanu <heyshantu13@gmail.com>
2020-03-04 18:22:44 +0530

    fixed fcm

--- config/app.php
+++ config/app.php
@@ -231,7 +231,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'FCM'      => LaravelFCM\Facades\FCM::class,
+        // 'FCM'      => LaravelFCM\Facades\FCM::class,
 
     ],
 
