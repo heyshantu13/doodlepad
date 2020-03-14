078be57146793396a5c93104c1c49edb5c17fcab
heyshantu13 <heyshantu13@gmail.com>
2020-03-14 20:09:36 +0530

    Fixed Reset Issue

--- routes/api.php
+++ routes/api.php
@@ -27,8 +27,8 @@ Route::group([
     Route::post('verifyOTP','AuthController@verifyOTP');
     Route::post('create-user', 'AuthController@signup');
     Route::post('create-profile', 'AuthController@createProfile')->middleware('auth:api');
-    Route::post('reset', 'AuthController@resetPassword')->middleware('throttle:4,10');
-    Route::post('new-password', 'AuthController@newPassword')->middleware('throttle:5,10');
+    Route::post('reset', 'AuthController@resetPassword')->middleware('throttle:5,5');
+    Route::post('new-password', 'AuthController@newPassword')->middleware('throttle:5,5');
     Route::get('user', 'AuthController@user')->middleware('auth:api');
     Route::get('checksession','AuthController@checksession')->middleware('auth:api');
 
