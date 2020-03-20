ebc312e23722c0b839eac991b2f9ed07c2b3bbd2
Shantanu Kulkarni <36811956+heyshantu13@users.noreply.github.com>
2020-03-20 22:58:22 +0530

    Update Constants

--- config/constants.php
+++ config/constants.php
@@ -1,25 +1,29 @@
 <?php
+
+//  Author: Shantanu K
+
 const GENDER = ['MALE' => 'male', 'FEMALE' => 'female'];
 const POST_TYPE = ['TEXT' => 'text', 'IMAGE' => 'image', 'VIDEO' => 'video', 'AUDIO' => 'audio', 'DOODLE' => 'doodle'];
 const POST_ACTIVITY_TYPE = ['COMMENT' => 'comment', 'LIKE' => 'like', 'DISLIKE' => 'dislike', 'FOLLOW' => 'follow'];
 const COMMENT_ACTIVITY_TYPE = ['LIKE' => 'like', 'REPLY' => 'reply'];
-// const COMMENT_ACTIVITY_REPLY = ['REPLY'=>'reply'];
+const COMMENT_ACTIVITY_REPLY = ['REPLY'=>'reply'];
 const ALIGNMENT = ['left'=>'left','right'=>'right','center'=>'center'];
-const FOLLOWTYPE = ['REQUESTED'=>'REQUESTED','FOLLOWING'=>'FOLLOWING'];
+const FOLLOWTYPE = ['REQUESTED'=>'REQUESTED','FOLLOWING'=>'FOLLOWING','APPROVED'=>'APPROVED'];
 return [
     'MALE' => GENDER['MALE'],
     'FEMALE' => GENDER['FEMALE'],
     'POST_TYPE_TEXT' => POST_TYPE['TEXT'],
     'POST_TYPE_IMAGE' => POST_TYPE['IMAGE'],
     'POST_TYPE_VIDEO' => POST_TYPE['VIDEO'],
-    'POST_TYPE_VIDEO' => POST_TYPE['AUDIO'],
-    'POST_TYPE_VIDEO' => POST_TYPE['DOODLE'],
+    'POST_TYPE_AUDIO' => POST_TYPE['AUDIO'],
+    'POST_TYPE_DOODLE' => POST_TYPE['DOODLE'],
     'POST_ACTIVITY_LIKE' => POST_ACTIVITY_TYPE['LIKE'],
     'POST_ACTIVITY_COMMENT' => POST_ACTIVITY_TYPE['COMMENT'],
     'COMMENT_ACTIVITY_LIKE' => COMMENT_ACTIVITY_TYPE['LIKE'],
     'COMMENT_ACTIVITY_REPLY' => COMMENT_ACTIVITY_TYPE['REPLY'],
     'USER_FOLLOW_REQUESTED' => FOLLOWTYPE['REQUESTED'],
     'USER_FOLLOW_FOLLOWING' => FOLLOWTYPE['FOLLOWING'],
+    'USER_FOLLOW_APPROVED' => FOLLOWTYPE['APPROVED'],
 
     'enums' => [
         'text_post'=>'TEXT',
