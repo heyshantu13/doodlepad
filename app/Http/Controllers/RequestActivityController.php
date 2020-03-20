bcfc8107037c287dc10a4849ea99ee93d3e2eec0
Shantanu Kulkarni <36811956+heyshantu13@users.noreply.github.com>
2020-03-20 22:56:37 +0530

    Added Requests API

--- app/Http/Controllers/RequestActivityController.php
+++ app/Http/Controllers/RequestActivityController.php
@@ -3,19 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\RequestActivity;
-use App\UserProfile;
-use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use DB;
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
+
 
 class RequestActivityController extends Controller
 {
     public function index()
     {
         $user = Auth::user();
-        $profile = UserProfile::where('user_id', $user->id)
-        ->firstOrFail();
-        $user = User:where('id',$user->id)->firstOrFail();
+
+    $users = DB::table('request_activities')
+        ->join('users', 'request_activities.follower_id', '=', 'users.id')
+        ->join('user_profiles','request_activities.follower_id','=','user_profiles.user_id')
+        ->select('users.id','users.username','request_activities.type','user_profiles.profile_picture_url','request_activities.created_at')
+        ->paginate(config('constants.paginate_per_page'));
+     return response()->json($users,200);
+
+
+}
+
+
+    public function acceptrequest(int $id){
+      
+    }
+
 
 }
