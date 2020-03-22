750948fe8e97b5581f132d8c39c6a732953ddd4c
Shantanu Kulkarni <36811956+heyshantu13@users.noreply.github.com>
2020-03-22 17:25:16 +0530

    Update RequestActivityController.php

--- app/Http/Controllers/RequestActivityController.php
+++ app/Http/Controllers/RequestActivityController.php
@@ -19,7 +19,7 @@ class RequestActivityController extends Controller
     $users = DB::table('request_activities')
         ->join('users', 'request_activities.follower_id', '=', 'users.id')
         ->join('user_profiles','request_activities.follower_id','=','user_profiles.user_id')
-        ->select('users.id','users.fullname','users.username','request_activities.type','user_profiles.profile_picture_url','request_activities.created_at')
+        ->select('request_activities.id','users.id','users.fullname','users.username','request_activities.type','user_profiles.profile_picture_url','request_activities.created_at')
         ->paginate(config('constants.paginate_per_page'));
      return response()->json($users,200);
 
