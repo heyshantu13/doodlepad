ae405a5920143f1e7529a518087d6f212f0857df
heyshantu13 <heyshantu13@gmail.com>
2020-04-27 11:57:17 +0530

    Fixed pinned and improve code

--- app/Console/Commands/NotifyToPinnedPost.php
+++ app/Console/Commands/NotifyToPinnedPost.php
@@ -48,7 +48,7 @@ class NotifyToPinnedPost extends Command
     {
        $results = DB::table('posts')->select('id','user_profile_id')
 ->where('is_pinned',0)
-->where('created_at', '<', Carbon::now()->subHours(1)->toDateTimeString())->get();
+->where('created_at', '<', Carbon::now()->subHours(23)->toDateTimeString())->get(['id','user_profile_id']);
 
 foreach ($results as $result) {
    PostHelper::createNotifyActivity($result->user_profile_id,$result->id);
