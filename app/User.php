a7cd9140acd0f8b05234a518fe5850c0803486fd
Shantanu <heyshantu13@gmail.com>
2020-08-16 12:40:56 +0530

    Fixed Fillable in User Model

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'fullname', 'email', 'password','mobile','username'
+        'fullname', 'email', 'password','mobile','username','country_code'
     ];
 
     /**
@@ -26,7 +26,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token','mobile'
     ];
 
     /**
