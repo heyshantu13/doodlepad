722a5c07ffb939194ff83aa20ec69000009127ee
Shantanu <heyshantu13@gmail.com>
2020-09-16 18:53:08 +0530

    Fixed signup issue

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -154,7 +154,7 @@ class AuthController extends Controller
 
            $profile->user_id = Auth::user()->id;
            $userProperties = [
-            'phoneNumber'=>Auth::user()->country_id.Auth::user()->mobile,
+            'phoneNumber'=>"+91".Auth::user()->mobile,
             'uid'=>Auth::user()->id ,
             'displayName' => Auth::user()->username,
             'photoUrl' =>env('AWS_URL')."/".$filePath,
